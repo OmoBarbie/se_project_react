2703c362665fd171206ca2c109a33626ae2c714e
Tayo <omotayo_dnl@yahoo.com>
2025-12-29 10:41:20 -0600

    Resolve merge conflicts (keep local project files)

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./Header.jsx";
+import Main from "./Main";
+import Animal from "./Animal";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Animal selectedAnimal="dog" />
+    </>
+  );
+}
+
+export default App;
