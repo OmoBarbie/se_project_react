1a6d9a2b9b5ed84974220878c8741f091e11fc36
Tayo <omotayo_dnl@yahoo.com>
2026-01-11 19:42:38 -0600

    Made styling changes to match figma design

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -10,7 +10,7 @@ function Main({ weatherData, clothingItems, onCardClick }) {
       <WeatherCard weatherData={weatherData} />
       <section className="cards">
         <p className="cards__text">
-          Today is {weatherData.temp.F} &deg; F/ You may want to wear:
+          Today is {Math.round(weatherData.temp.F)}&deg;F/ You may want to wear:
         </p>
         {/* TODO add the cards */}
         <ul className="cards__list">
