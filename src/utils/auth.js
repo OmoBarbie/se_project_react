a3163cd68ecbcf61bbcaa3f364f3a0ee7a07f4c3
Tayo <omotayo_dnl@yahoo.com>
2026-03-30 20:22:58 -0500

    register, signup, authorize, login, checkToken autologin on refresh

---
+++ src/utils/auth.js
@@ -0,0 +1,43 @@
+const baseUrl = "http://localhost:3001";
+
+// SIGN UP
+export const register = ({ name, avatar, email, password }) => {
+  return fetch(`${baseUrl}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, avatar, email, password }),
+  }).then((res) => {
+    if (!res.ok) return Promise.reject(`Error: ${res.status}`);
+    return res.json();
+  });
+};
+
+// SIGN IN
+export const authorize = ({ email, password }) => {
+  return fetch(`${baseUrl}/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then((res) => {
+    if (!res.ok) return Promise.reject(`Error: ${res.status}`);
+    return res.json();
+  });
+};
+
+// CHECK TOKEN
+export const checkToken = (token) => {
+  return fetch(`${baseUrl}/users/me`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  }).then((res) => {
+    if (!res.ok) return Promise.reject(`Error: ${res.status}`);
+    return res.json();
+  });
+};
